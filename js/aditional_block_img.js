99f016d3a68b29723c2d7be48125c80a8c9c1e7f
Leonid <leonideko1@gmail.com>
2023-02-27 12:41:42 +0200

    clock opacity block adition

--- js/aditional_block_img.js
+++ js/aditional_block_img.js
@@ -1,26 +1,25 @@
-
-// const img = document.querySelector('#additional-container-img');
-
-
-// let opacity = 0.3;
-// let direction = 'up';
-
-// (() => { 
-// setInterval(function () {
-
-//     if (direction === 'up') {
-//         opacity += 0.01;
-//         if (opacity >= 1) {
-//             direction = 'down';
-//         }
-//     } else {
-//         opacity -= 0.02;
-//         if (opacity <= 0.2) {
-//             direction = 'up';
-//         }
-//     }
+const img = document.querySelector('#additional_clock_opacity');
 
 
-//     img.style.opacity = opacity;
-// }, 60);
-// })();
+let opacity = 0.3;
+let direction = 'up';
+
+(() => { 
+setInterval(function () {
+
+    if (direction === 'up') {
+        opacity += 0.01;
+        if (opacity >= 1) {
+            direction = 'down';
+        }
+    } else {
+        opacity -= 0.02;
+        if (opacity <= 0.2) {
+            direction = 'up';
+        }
+    }
+
+
+    img.style.opacity = opacity;
+}, 60);
+})();
