1b6e2015835456ec5d0cec5ad570a5f4e8e76750
Roman Sitko <53574854+romkomp@users.noreply.github.com>
2023-03-06 20:24:00 +0000

    worked in team carousel. Add team waves

--- js/team_carousel.js
+++ js/team_carousel.js
@@ -1,9 +1,39 @@
 (() => {
+
+    const socials_logos = `<div class="carousel__social-logos">
+    <a href="https://www.facebook.com" class="carousel__social-logo-item"><img
+            class="social-logo-img" src="img/logo-facebook.png" alt="Facebook logo"></a>
+    <a href="https://www.instagram.com/" class="carousel__social-logo-item"><img
+            class="social-logo-img" src="img/logo-insta.png" alt="Instagram logo"></a>
+    <a href="https://www.youtube.com/" class="carousel__social-logo-item"><img
+            class="social-logo-img" src="img/logo-youtube.png" alt="Youtube logo"></a>
+    </div>`;
+
     const slides = [
-        '<div class="team_item"><img class="team__foto" src="img/team-dan.jpg" alt="Member of team Dan Pitts"><div class="carousel__member-name">Dan Pitts</div></div>',
-        '<div class="team_item"><img class="team__foto" src="img/team-helena.jpg" alt="Member of team Helena Farse"><div class="carousel__member-name">Helena Farse</div></div>',
-        '<div class="team_item"><img class="team__foto" src="img/team-lucy.jpg" alt="Member of team Lucy Madins"><div class="carousel__member-name">Lucy Madins</div></div>',
-        '<div class="team_item"><img class="team__foto" src="img/team-tomas.jpg" alt="Member of team Tomas Willens"><div class="carousel__member-name">Tomas Willens</div></div>'
+        `<div class="team_item">
+            <img class="team__foto" src="img/team-dan.jpg" alt="Member of team Dan Pitts">
+            <div class="carousel__member-name">Dan Pitts</div>
+            <p class="carousel__member_job">Professional guide | 5 years of experiance</p>
+            ${socials_logos}
+        </div>`,
+        `<div class="team_item">
+            <img class="team__foto" src="img/team-helena.jpg" alt="Member of team Helena Farse">
+            <div class="carousel__member-name">Helena Farse</div>
+            <p class="carousel__member_job">Professional guide | 10 years of experiance</p>
+            ${socials_logos}
+        </div>`,
+        `<div class="team_item">
+            <img class="team__foto" src="img/team-lucy.jpg" alt="Member of team Lucy Madins">
+            <div class="carousel__member-name">Lucy Madins</div>
+            <p class="carousel__member_job">Photographer | 5 years of experiance</p>
+            ${socials_logos}
+        </div>`,
+        `<div class="team_item">
+            <img class="team__foto" src="img/team-tomas.jpg" alt="Member of team Tomas Willens">
+            <div class="carousel__member-name">Tomas Willens</div>
+            <p class="carousel__member_job">Photographer | 5 years of experiance</p>
+            ${socials_logos}
+        </div>`
     ];
 
 
@@ -15,9 +45,10 @@
         if (window.innerWidth > 768) {
             const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
             slidesContainer.innerHTML += slides[secondSlide];
-            if (window.innerHTML > 900) {
+            if (window.innerWidth > 900) {
                 const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
                 slidesContainer.innerHTML += slides[thirdSlide];
+                console.log(slides.length);
             }
         }
     }
