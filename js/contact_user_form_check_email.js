abd1279fb254d0944f51e25ac4ff64e2ecd96b61
Kateryna Iefimova <90828019+KaterynaIefimova@users.noreply.github.com>
2023-03-07 23:38:19 +0000

    revise contact_user_form_check_email.js

--- js/contact_user_form_check_email.js
+++ js/contact_user_form_check_email.js
@@ -1,8 +1,20 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
 function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(input.value.trim())) {
-      showSuccess(input);
-    } else {
-      showError(input, 'Email is not valid');
-    }
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
   }
+}
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    if(checkRequired(email)){
+      checkEmail(email);
+  
+    }
+  
+  });
